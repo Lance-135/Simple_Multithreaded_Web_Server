9646cf8e73fadd5de0f6704369e41d3e06b85343
Lance <austinkarki84@gmail.com>
2025-03-20 23:45:06 +0545

    using different approaches to make our web server multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use std::{
-    fs,
+    {fs, thread},
+    time:: Duration,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
+use hello::ThreadPool;
 
 fn main(){
     let reader = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let thread_pool = ThreadPool::new(5);
     for stream in reader.incoming(){
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread_pool.execute( ||{
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,11 +22,14 @@ fn handle_connection(mut stream: TcpStream){
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1"{
+    let (status_line, filename) = match &request_line[..]{
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 Ok", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(20));
             ("HTTP/1.1 200 OK", "hello.html")
-        }else{
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
-        };
+        },
+        _=> ("HTTP/1.1 404 NOT Found", "404.html")
+    };
     
         let content = fs::read_to_string(filename).unwrap();
         let length = content.len();
