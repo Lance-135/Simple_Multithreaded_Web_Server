c2872a462e700ba69091a00060074688f805a4d4
Lance <austinkarki84@gmail.com>
2025-03-20 21:15:32 +0545

    refactored the handle_connection function to reduce repetition of code

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,15 @@ fn main(){
 fn handle_connection(mut stream: TcpStream){
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    if request_line == "GET / HTTP/1.1"{
-        let status_line = "HTTP/1.1 200 OK";
-        let content = fs::read_to_string("hello.html").unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1"{
+            ("HTTP/1.1 200 OK", "hello.html")
+        }else{
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+    
+        let content = fs::read_to_string(filename).unwrap();
         let length = content.len();
         let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
         stream.write_all(response.as_bytes()).unwrap();
-    }
-    else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let content = fs::read_to_string("404.html").unwrap();
-        let length = content.len();
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{content}"
-        );
-        stream.write_all(response.as_bytes()).unwrap();
-    }
 }
